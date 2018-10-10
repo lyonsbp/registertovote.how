fe6cd2d9435b7f7bcfe3256293c91fb19c5ddd8a
Dave Follett <davefollet@gmail.com>
2018-10-09 23:04:01 -0400

    WIP: added Map component

---
+++ src/components/Map.vue
@@ -0,0 +1,44 @@
+<template>
+  <GChart
+    type="GeoChart"
+    :data="chartData"
+    :options="chartOptions"
+    :events="chartEvents"
+  />
+</template>
+
+<script>
+
+import { GChart } from 'vue-google-charts'
+
+export default {
+  components: {
+    GChart
+  },
+  data () {
+    return {
+      placehoolder: null,
+      chartData: [
+        ['State', 'Select'],
+        ['US-MI', 0],
+      ],
+      chartOptions: {
+        region: 'US',
+        displayMode: 'regions',
+        resolution: 'provinces',
+      },
+      chartEvents: {
+        'regionClick': (region) => {
+          console.log(region)
+        }
+      }
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
