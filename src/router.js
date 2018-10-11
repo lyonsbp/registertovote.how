cc1ca76ca04429cce2580e75a9bb7a3d9f0a3688
Brandon Lyons <lyonsbp@umich.edu>
2018-10-10 23:53:08 -0400

    Revert "Revert "Google charts""

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
+import Map from './views/Map.vue'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/about',
       name: 'about',
       component: About
+    },
+    {
+      path: '/map',
+      name: 'map',
+      component: Map
     }
   ]
 })
